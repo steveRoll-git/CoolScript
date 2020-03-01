4a0d7ba6392e42a2e51a5aa8cbc92e94d187f54c
Ron Tseytlin <ronts390@gmail.com>
2020-03-01 20:52:19 +0200

    added debug file

---
+++ CoolLanguage/VM/Debug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolLanguage.VM
+{
+    static class Debug
+    {
+        public static void PrintInstructions(VMInstruction[] instructions)
+        {
+            foreach (VMInstruction instruction in instructions)
+            {
+                Console.WriteLine(instruction.type + "\t" + instruction.data);
+            }
+        }
+    }
+}
